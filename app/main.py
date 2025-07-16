92ef1a85b2dfa7650d8c08a1b391bdf1625b3380
lhchun <man.hin.kwan@prudential.com.hk>
2025-07-17 00:16:39 +0800

    CORSMiddleware

--- app/main.py
+++ app/main.py
@@ -1,9 +1,20 @@
 from fastapi import FastAPI
 from app.api import routes
+from fastapi.middleware.cors import CORSMiddleware
+
 
 def create_app() -> FastAPI:
     app = FastAPI(title="FastAPI API Utility Layer")
     app.include_router(routes.router)
+    origins = ["*"]
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+
     return app
 
 app = create_app()
